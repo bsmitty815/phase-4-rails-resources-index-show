fffb4b0685a33e5517452af0285f03ec19b9df52
Brian Smith <brian.bubbam@gmail.com>
2021-08-10 14:30:20 -0500

    commit tree da73527d57b581eb89afa50d0add7c12bd51d1d0

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  # get '/birds', to: 'birds#index'
+  # get '/bidrs/:id', to: 'birds#show'
+  
+  # terminal - rails routes
+  # this will show you all the restful routes rails has for you
+
+  resources :birds, only: [:index, :show]
+
 end
